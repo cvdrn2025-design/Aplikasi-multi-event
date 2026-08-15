f8dc535eccac3ca830f7f87578967d27c3dda58e
cvdrn2025-design <cv.drn2025@gmail.com>
2026-08-15 14:19:00 +0800

    Add sports configuration data for multiple sports

--- events/sports/sports-data.js
+++ events/sports/sports-data.js
@@ -1,1 +1,23 @@
+export const sportsConfig = {
+  football: {
+    name: "Sepak Bola / Futsal",
+    formats: ["Single Elimination", "Setengah Kompetisi", "Fase Grup + Knockout"],
+    subCategories: ["Futsal Putra", "Futsal Putri", "Sepak Bola Open"]
+  },
+  basketball: {
+    name: "Bola Basket",
+    formats: ["Single Elimination", "Double Elimination"],
+    subCategories: ["3x3 Tournament", "5v5 Standard"]
+  },
+  badminton: {
+    name: "Bulu Tangkis",
+    formats: ["Best of 3 (Gugur)", "Setengah Kompetisi"],
+    subCategories: ["Tunggal Putra", "Tunggal Putri", "Ganda Putra", "Ganda Putri", "Ganda Campuran"]
+  },
+  esports: {
+    name: "E-Sports",
+    formats: ["Single Elimination", "Swiss Stage"],
+    subCategories: ["Mobile Legends", "PUBG Mobile", "Valorant"]
+  }
+};
 
