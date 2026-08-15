44c769cb18ed567f3a9ff5416a5a82ed0e391e8a
cvdrn2025-design <cv.drn2025@gmail.com>
2026-08-15 16:10:58 +0800

    Refactor sportsConfig and add volleyball category
    Updated sportsConfig to use spread operator for football and basketball, and added volleyball category.

--- events/sports/sports-data.js
+++ events/sports/sports-data.js
@@ -1,13 +1,9 @@
 export const sportsConfig = {
-  football: {
-    name: "Sepak Bola / Futsal",
-    formats: ["Single Elimination", "Setengah Kompetisi"],
-    subCategories: ["Futsal Putra", "Futsal Putri"]
-  },
-  basketball: {
-    name: "Bola Basket",
-    formats: ["Single Elimination", "Double Elimination"],
-    subCategories: ["3x3", "5v5"]
+  football: { ... },
+  basketball: { ... },
+  volleyball: {
+    name: "Bola Voli",
+    formats: ["Single Elimination", "Round Robin"],
+    subCategories: ["Putra", "Putri", "Campuran"]
   }
 };
-
